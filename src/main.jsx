898838d0dfd5cc0f4b09be37846e770194907a26
Bohdan <bosrok.ke@gmail.com>
2025-03-03 15:17:49 +0000

    BrowserRouter Fix F5

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
@@ -7,11 +8,13 @@ import { ModalProvider } from "./context/ModalContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <AuthProvider>
-      <ModalProvider>
-        <App />
-      </ModalProvider>
-    </AuthProvider>
-  </Provider>
+  <BrowserRouter basename="/Nanny-Services">
+    <Provider store={store}>
+      <AuthProvider>
+        <ModalProvider>
+          <App />
+        </ModalProvider>
+      </AuthProvider>
+    </Provider>
+  </BrowserRouter>
 );
